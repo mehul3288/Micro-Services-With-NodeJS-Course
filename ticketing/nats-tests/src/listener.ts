ab535566c7d81d81db1d8734e4d5c7d59fe152a7
mehul3288 <mehulr286@gmail.com>
2026-09-23 13:24:55 +0530

    Section 15 (Connecting to Nats in a Node JS World)

--- ticketing/nats-tests/src/listener.ts
+++ ticketing/nats-tests/src/listener.ts
@@ -1,6 +1,43 @@
-import nats, { Message } from "node-nats-streaming";
+import nats, { Message, Stan } from "node-nats-streaming";
 import { randomBytes } from "node:crypto";
-console.clear();
+import { TicketCreatedListener } from "./events/ticket-created-listener";
+import { Subjects } from "./events/subjects";
+//Just tested code:
+// console.clear();
+// const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
+//     url: "http://localhost:4222"
+// });
+
+// stan.on('connect', () => {
+//     console.log("connected to nats");
+//     stan.on("close", () => {
+//         console.log("Nats Connection closed!");
+//         process.exit();
+
+//     })
+//     const options = stan
+//         .subscriptionOptions()
+//         .setManualAckMode(true)
+//         .setDeliverAllAvailable() //It will send all the events delivered in the past 
+//         .setDurableName("orders-service") //It will
+//     const subscription = stan.subscribe("ticket:created", "orders-service-queue-group", options);
+//     // const subscription = stan.subscribe("ticket:created", options);
+//     subscription.on('message', (msg: Message) => {
+//         console.log("Message received");
+//         const data = msg.getData();
+//         if (typeof data === "string") {
+//             console.log("Received event #" + msg.getSequence() + ":", data);
+//         }
+//         msg.ack();
+//         // console.log(msg.getData());
+//     });
+// });
+
+// //Interrupt signal when we try to close the terminal
+// process.on("SIGINT", () => stan.close());
+// process.on("SIGTERM", () => stan.close());
+
+
 const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
     url: "http://localhost:4222"
 });
@@ -12,24 +49,12 @@ stan.on('connect', () => {
         process.exit();
 
     })
-    const options = stan
-        .subscriptionOptions()
-        .setManualAckMode(true)
-        .setDeliverAllAvailable() //It will send all the events delivered in the past 
-        .setDurableName("orders-service") //It will
-    const subscription = stan.subscribe("ticket:created", "orders-service-queue-group", options);
-    // const subscription = stan.subscribe("ticket:created", options);
-    subscription.on('message', (msg: Message) => {
-        console.log("Message received");
-        const data = msg.getData();
-        if (typeof data === "string") {
-            console.log("Received event #" + msg.getSequence() + ":", data);
-        }
-        msg.ack();
-        // console.log(msg.getData());
-    });
+    new TicketCreatedListener(stan).listen();
 });
 
 //Interrupt signal when we try to close the terminal
 process.on("SIGINT", () => stan.close());
 process.on("SIGTERM", () => stan.close());
+
+
+
--- ticketing/nats-tests/src/publisher.ts
+++ ticketing/nats-tests/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from "node-nats-streaming"
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 
 console.clear();
 
@@ -6,16 +7,27 @@ const stan = nats.connect("ticketing", "abc", {
     url: "http://localhost:4222"
 });
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
     console.log("Connected to NATS");
 
-    const data = {
-        id: "123",
-        title: "concert",
-        price: 99
-    };
+    const publisher = new TicketCreatedPublisher(stan);
+    try {
+        await publisher.publish({
+            id: "123",
+            title: "concert",
+            price: 919
+        });
+    } catch (error) {
+        console.error(error);
+    }
 
-    stan.publish("ticket:created", JSON.stringify(data), () => {
-        console.log("Event published");
-    });
+    // const data = {
+    //     id: "123",
+    //     title: "concert",
+    //     price: 99
+    // };
+
+    // stan.publish("ticket:created", JSON.stringify(data), () => {
+    //     console.log("Event published");
+    // });
 });
